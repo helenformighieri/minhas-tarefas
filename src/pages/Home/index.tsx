ef6c349929dddd89c40a0007d98aa5648da43aeb
Helen <helendianaf@gmail.com>
2024-07-03 11:25:31 -0300

    first commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import BotaoAdicionar from "../../components/BotaoAdicionar";
+import BarraLateral from "../../containers/BarraLateral";
+import ListaDeTarefas from "../../containers/ListaDeTarefa";
+
+const Home = () => (
+    <>
+        <BarraLateral mostrarFiltros={true}/>
+        <ListaDeTarefas/>
+        <BotaoAdicionar/>
+    </>
+)
+
+export default Home
